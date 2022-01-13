bce175e6d119e3f7c63d9498c1cff1539f6624f9
zlatanglor <oshovskiy.d@gmail.com>
2022-01-13 15:20:46 +0200

    add files

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        loop:true,
+        nav:true,
+        dots: false,
+        autoplay:false,
+        responsive:{
+            0:{
+                items:1
+            }
+        }
+    });
+});
